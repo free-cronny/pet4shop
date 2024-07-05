db7b9ddb2400d4c07bdd270cde17047b0e55fdba
Igor Marines <igor.marines25@gmail.com>
2024-07-05 13:18:24 -0300

    feature: :sparkles: Criado role de migrate de animal

---
+++ database/migrations/2024_07_05_152928_create_animais_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animais', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('email', 100);
+            $table->string('cpf', 11);
+            $table->string('telefone', 11);
+            $table->string('nome_animal', 100);
+            $table->string('raca_animal', 100);
+            $table->string('servico', 100);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animais');
+    }
+};
